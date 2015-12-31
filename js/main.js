42e0922413785172264e2e42496300b8379e40ae
Ahmad Hakroosh <ahmad@ahmad-h.com>
2015-12-31 18:17:44 +0200

    added hoverable drop-down menu

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//General function to select HTML objects into the document
+function $ (selector) {
+	return document.querySelector(selector);
+}
+
+var dropdowns = { 
+	/**
+	* Add a 'mouseover' and 'keydown' events to open / close dropdowns
+	*/
+	init : function () {
+		
+	},
+
+	closeDropdown : function () {
+
+	},
+
+	openDropdown : function () {
+
+	}
+};
