e2537de0b969b483a8014f83137e96378f939a70
Ahmad Hakroosh <ahmad@ahmad-h.com>
2016-01-01 22:34:41 +0200

    added tabs functionality, frame layout, and icons

--- js/main.js
+++ js/main.js
@@ -1,36 +1,64 @@
-//General function to select HTML objects into the document
+//General function to select DOM elements
 function $ (selector) {
 	return document.querySelector(selector);
 }
 
-var dropdowns = { 
-	/**
-	* Add a 'mouseover' and 'keydown' events to open / close dropdowns
-	*/
-	init : function () {
-		
-	},
+function all (selector) {
+	return document.querySelectorAll(selector);
+}
 
-	closeDropdown : function () {
+var reports = [
+	{name : "Corporate", url : ""},
+	{name : "Simple", url : ""},
+	{name : "Business", url : ""}
+];
 
-	},
+var dashboards = [
+	{name : "Google", url : "https://www.google.com"},
+	{name : "Yahoo", url : "https://www.yahoo.com"},
+	{name : "Microsoft", url : "https://www.microsoft.com"}
+];
 
-	openDropdown : function () {
+var guides = [
+	{name : "Stack Overflow", url : "https://www.stackoverflow.com"},
+	{name : "W3Schools", url : "https://www.w3schools.com"}
+];
 
+//set the links of each menu
+function setLinks (linksHeader, linksList) {
+	var listSelector = "." + linksHeader + " .menu .action-list";
+	var link = "";
+	for (var i = 0; i < linksList.length; i++) {
+		link = "<li class=\"action-list-item\"><a href=\"" + linksList[i].url + "\">" + linksList[i].name + "</a></li>";
+		$(listSelector).innerHTML += link;
+	};
+}
+
+//initialize the application view
+function initialize () {
+	//set links of each menu
+	setLinks("reports", reports);
+	setLinks("dashboards", dashboards);
+	setLinks("guides", guides);
+
+	tabbing();
+	$(".tab-item").className = "active-tab-item";
+
+	console.log("hello");
+}
+
+//activate selected tab, deactivate others
+function tabbing () {
+	//add an event listener for each tab
+	var tabs = all(".tab-item a");
+
+	for (var i = 0; i < tabs.length; i++) {
+		tabs[i].addEventListener("click", function (e) {
+			$(".active-tab-item").className = "tab-item";
+			this.parentNode.className = "active-tab-item";
+			console.log("hello");
+		});
 	}
 }
 
-/*function notify () {
-	var request = new XMLHttpRequest();
-	request.open("GET", "./data/config.json");
-	request.send();
-
-	request.onsreadytatechange = function () {
-		if (request.readyState == 4 && request.status == 200) {
-			$(".notifications").innerHTML = request.responseText.;
-		};
-	}
-}
-
-notify();
-console.log("hello");*/
+window.onLoad = initialize();
