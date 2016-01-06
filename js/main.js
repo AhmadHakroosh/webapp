bf8302f38af4e00429dcdd3392dc6b63bdd9a779
Ahmad Hakroosh <ahmad@ahmad-h.com>
2016-01-06 22:03:10 +0200

    fixed initialization function, separated commands to functions and some other little fixes

--- js/main.js
+++ js/main.js
@@ -7,18 +7,6 @@ function all (selector) {
 	return document.querySelectorAll(selector);
 }
 
-//initialize the application view
-function initialize () {
-	//get the data from the server
-	requestData();
-	//activate tabbing functionality
-	tabbing();
-	//set links list
-	setLinksList();
-	//set frame link
-	setFrameLink();
-}
-
 //activate selected tab, deactivate others
 function tabbing () {
 	//add an event listener for each tab
@@ -28,8 +16,10 @@ function tabbing () {
 		tabs[i].addEventListener("click", function (e) {
 			$(".active-tab-item").className = "tab-item";
 			this.parentNode.className = "active-tab-item";
+
 			setLinksList();
 			setFrameLink();
+
 			//hide settings icon and links lists for My Folders tab
 			if (this.hash == "#my-folders") {
 				$("#settings").style.display = "none";
@@ -54,54 +44,34 @@ function requestData () {
 		if (request.readyState == 4 && request.status == 200) {
 			var data = JSON.parse(request.responseText);
 			
-			$(".notifications").innerHTML = data.notification;
+			//set notification
+			setNotification(data.notification);
 
 			//set nav-sections titles
-			var navSections = all(".nav-section");
-			for (var i = 0; i < navSections.length; i++) {
-				navSections[i].innerHTML = "<p>" + data.quickActions[i].label + "</p>" + navSections[i].innerHTML;
-				navSections[i].style.background = "black url(./img/icons/" + data.quickActions[i].icon + ".png) center top 60px no-repeat";
-			}
-			//set menu-caption titles
-			var menuCaptions = all(".menu-caption");
-			for (var i = 0; i < menuCaptions.length; i++) {
-				menuCaptions[i].innerHTML = "<p>" + data.quickActions[i].actionsLabel + "</p>";
-			}
-			//set links for quick actions menus
-			var actionLists = all(".action-list");
-			for (var i = 0; i < actionLists.length; i++) {
-				actions = data.quickActions[i].actions;
-				for (var j = 0; j < actions.length; j++) {
-					actionLists[i].innerHTML += "<li class=\"action-list-item\"><a href=\"" + actions[j].url + "\">" + actions[j].label + "</a></li>"
-				}
-			}
-			//initialize tabs data
-			var tabs = all(".tab-link");
-			var tabsList = data.tabsList;
-			for (var i = 0; i < tabs.length; i++) {
-				tabs[i].innerHTML = "<i class=\"" + tabsList[i].icon + "\"></i> " + tabsList[i].label;
-			}
+			setMenus(data.quickActions);
 
-			//set tabs links
-			var links;
-			for (var i = 0; i < data.tabsList.length; i++) {
-				links = data.tabsList[i].links;
-				for (var j = 0; j < links.length; j++) {
-					$(data.tabsList[i].hash + " .links").innerHTML += "<li class=\"link-item\"><a href=\"" + links[j].url + "\">" + links[j].label + "</a></li>";
-				}
-			}
+			//initialize tabs data
+			initializeTabs(data.tabsList);
+
+			//set links list
+			setLinksList();
 		}
 	};
 }
 
 function saveData () {
-
+	var formRows = all(".settings-form .row");
+	var rows = [];
+	for (var i = 0; i < formRows.length; i++) {
+		
+	};
 }
 
 function setLinksList () {
 	var activeTab = $(".active-tab-item .tab-link").hash;
 	$(".links-action-list").innerHTML = $(activeTab + " .links").innerHTML;
 	$(".active-link").innerHTML = $(activeTab + " .links .link-item").innerHTML;
+	setFrameLink();
 }
 
 function setFrameLink () {
@@ -111,6 +81,60 @@ function setFrameLink () {
 
 function setExpandLink () {
 	$(".expand-icon").href = $(".frame-window").src;
+}
+
+function setNotification (notification) {
+	$(".notifications").innerHTML = notification;
+}
+
+function setMenus (quickActions) {
+	//set menus backgrounds and headers
+	var navSections = all(".nav-section");
+	for (var i = 0; i < navSections.length; i++) {
+		navSections[i].innerHTML = "<p>" + quickActions[i].label + "</p>" + navSections[i].innerHTML;
+		navSections[i].style.background = "black url(./img/icons/" + quickActions[i].icon + ".png) center top 60px no-repeat";
+	}
+
+	//set menu captions
+	var menuCaptions = all(".menu-caption");
+	for (var i = 0; i < menuCaptions.length; i++) {
+		menuCaptions[i].innerHTML = "<p>" + quickActions[i].actionsLabel + "</p>";
+	}
+
+	//set links for quick actions menus
+	var actionLists = all(".action-list");
+	for (var i = 0; i < actionLists.length; i++) {
+		actions = quickActions[i].actions;
+		for (var j = 0; j < actions.length; j++) {
+			actionLists[i].innerHTML += "<li class=\"action-list-item\"><a href=\"" + actions[j].url + "\">" + actions[j].label + "</a></li>"
+		}
+	}
+}
+
+function initializeTabs (tabsList) {
+	var tabs = all(".tab-link");
+	for (var i = 0; i < tabs.length; i++) {
+		tabs[i].innerHTML = "<i class=\"" + tabsList[i].icon + "\"></i> " + tabsList[i].label;
+	}
+
+	//set tabs links
+	var links;
+	for (var i = 0; i < tabsList.length; i++) {
+		links = tabsList[i].links;
+		for (var j = 0; j < links.length; j++) {
+			$(tabsList[i].hash + " .links").innerHTML += "<li class=\"link-item\"><a href=\"" + links[j].url + "\">" + links[j].label + "</a></li>";
+		}
+	}
+}
+
+//initialize the application view
+function initialize () {
+	//get the data from the server
+	requestData();
+	//activate tabbing functionality
+	tabbing();
+	//link event listener to save button
+	$("#form-save").addEventListener("click", saveData());
 }
 
 window.onLoad = initialize();
